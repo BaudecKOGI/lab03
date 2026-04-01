20076bf581a0f01dc2754bf7c3d1a9cd7b30afc0
BaudecKOGI <ucedadelacruza@gmail.com>
2026-04-01 16:58:07 -0500

    Agregando la vista StudentReport

---
+++ src/main/java/com/tecsup/lab3/StudentReport.java
@@ -0,0 +1,9 @@
+public package com.tecsup.lab3.view;
+
+public class StudentReport {
+    public static void main(String[] args) {
+        System.out.println("Reporte de estudiantes");
+    }
+} {
+    
+}
